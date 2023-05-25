8701e27f1affed014c614b622ff757e23482eed2
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-05-25 21:53:24 +0530

    learnt slices

---
+++ 8.Slices/slices/target/debug/deps/slices-b329ba4a2500a23a.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/8.Slices/slices/target/debug/deps/slices-b329ba4a2500a23a: src/main.rs
+
+/Users/praky/Projects/RUST-practise/8.Slices/slices/target/debug/deps/slices-b329ba4a2500a23a.d: src/main.rs
+
+src/main.rs:
---
+++ 8.Slices/slices/target/debug/slices.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/RUST-practise/8.Slices/slices/target/debug/slices: /Users/praky/Projects/RUST-practise/8.Slices/slices/src/main.rs
