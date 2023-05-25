983372a4892918c9ff63978f45afbe880de0b980
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-05-25 07:20:08 +0530

    sample while program

---
+++ 5.sampleProgram/sample/target/debug/deps/sample-9cd8a6fbac218885.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/5.sampleProgram/sample/target/debug/deps/sample-9cd8a6fbac218885: src/main.rs
+
+/Users/praky/Projects/RUST-practise/5.sampleProgram/sample/target/debug/deps/sample-9cd8a6fbac218885.d: src/main.rs
+
+src/main.rs:
---
+++ 5.sampleProgram/sample/target/debug/sample.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/RUST-practise/5.sampleProgram/sample/target/debug/sample: /Users/praky/Projects/RUST-practise/5.sampleProgram/sample/src/main.rs
