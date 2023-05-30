962068960e7d9b08158ca106aace8eb51a2c8ef2
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-05-30 23:11:06 +0530

    hw12 tuple destructuring

---
+++ 12.encode_homework/h12/target/debug/deps/h12-4ba2215e81084b9d.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/12.encode_homework/h12/target/debug/deps/h12-4ba2215e81084b9d: src/main.rs
+
+/Users/praky/Projects/RUST-practise/12.encode_homework/h12/target/debug/deps/h12-4ba2215e81084b9d.d: src/main.rs
+
+src/main.rs:
---
+++ 12.encode_homework/h12/target/debug/h12.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/RUST-practise/12.encode_homework/h12/target/debug/h12: /Users/praky/Projects/RUST-practise/12.encode_homework/h12/src/main.rs
