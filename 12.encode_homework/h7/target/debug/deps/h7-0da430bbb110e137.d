90f053dd78e4526d82f75dafef14fff4fd0edb1f
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-05-30 22:10:14 +0530

    homework 7 on if-else

---
+++ 12.encode_homework/h7/target/debug/deps/h7-0da430bbb110e137.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/12.encode_homework/h7/target/debug/deps/h7-0da430bbb110e137: src/main.rs
+
+/Users/praky/Projects/RUST-practise/12.encode_homework/h7/target/debug/deps/h7-0da430bbb110e137.d: src/main.rs
+
+src/main.rs:
---
+++ 12.encode_homework/h7/target/debug/h7.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/RUST-practise/12.encode_homework/h7/target/debug/h7: /Users/praky/Projects/RUST-practise/12.encode_homework/h7/src/main.rs
