2127c9fc67866326a77d95de55c7c7e52c0dfdd5
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-05-30 22:07:02 +0530

    homework 6 on if-else statements

---
+++ 12.encode_homework/h6/target/debug/deps/h6-330ff2fd4d8f93f5.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/12.encode_homework/h6/target/debug/deps/h6-330ff2fd4d8f93f5: src/main.rs
+
+/Users/praky/Projects/RUST-practise/12.encode_homework/h6/target/debug/deps/h6-330ff2fd4d8f93f5.d: src/main.rs
+
+src/main.rs:
---
+++ 12.encode_homework/h6/target/debug/h6.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/RUST-practise/12.encode_homework/h6/target/debug/h6: /Users/praky/Projects/RUST-practise/12.encode_homework/h6/src/main.rs
