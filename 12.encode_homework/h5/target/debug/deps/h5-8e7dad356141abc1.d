3c66ff2acef0806c27362a5c1393db8afb083bbd
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-05-30 22:02:57 +0530

    Homework 5

---
+++ 12.encode_homework/h5/target/debug/deps/h5-8e7dad356141abc1.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/12.encode_homework/h5/target/debug/deps/h5-8e7dad356141abc1: src/main.rs
+
+/Users/praky/Projects/RUST-practise/12.encode_homework/h5/target/debug/deps/h5-8e7dad356141abc1.d: src/main.rs
+
+src/main.rs:
---
+++ 12.encode_homework/h5/target/debug/h5.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/RUST-practise/12.encode_homework/h5/target/debug/h5: /Users/praky/Projects/RUST-practise/12.encode_homework/h5/src/main.rs
