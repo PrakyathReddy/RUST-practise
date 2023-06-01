a860e83b603e6fc2bf67f03acdb3fa5445577ddd
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-01 21:37:30 +0530

    LGR statics

---
+++ 4.constants/statics/target/debug/deps/statics-35b74d694f0b44b3.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/4.constants/statics/target/debug/deps/statics-35b74d694f0b44b3: src/main.rs
+
+/Users/praky/Projects/RUST-practise/4.constants/statics/target/debug/deps/statics-35b74d694f0b44b3.d: src/main.rs
+
+src/main.rs:
---
+++ 4.constants/statics/target/debug/statics.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/RUST-practise/4.constants/statics/target/debug/statics: /Users/praky/Projects/RUST-practise/4.constants/statics/src/main.rs
