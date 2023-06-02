b4e64d51ba1fa2986fc261ef2f90761901b31b6c
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-02 20:11:46 +0530

    cloning a variable

---
+++ 11.ownership/clone/target/debug/clone.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/RUST-practise/11.ownership/clone/target/debug/clone: /Users/praky/Projects/RUST-practise/11.ownership/clone/src/main.rs
---
+++ 11.ownership/clone/target/debug/deps/clone-d630f7ad30797eb9.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/11.ownership/clone/target/debug/deps/clone-d630f7ad30797eb9: src/main.rs
+
+/Users/praky/Projects/RUST-practise/11.ownership/clone/target/debug/deps/clone-d630f7ad30797eb9.d: src/main.rs
+
+src/main.rs:
