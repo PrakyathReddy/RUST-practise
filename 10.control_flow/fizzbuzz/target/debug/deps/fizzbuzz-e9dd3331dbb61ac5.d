06e9ff25705ff392b61bcbabc4efeab39f11ce55
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-02 14:49:38 +0530

    fizzbuzz

---
+++ 10.control_flow/fizzbuzz/target/debug/deps/fizzbuzz-e9dd3331dbb61ac5.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/10.control_flow/fizzbuzz/target/debug/deps/fizzbuzz-e9dd3331dbb61ac5: src/main.rs
+
+/Users/praky/Projects/RUST-practise/10.control_flow/fizzbuzz/target/debug/deps/fizzbuzz-e9dd3331dbb61ac5.d: src/main.rs
+
+src/main.rs:
---
+++ 10.control_flow/fizzbuzz/target/debug/fizzbuzz.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/RUST-practise/10.control_flow/fizzbuzz/target/debug/fizzbuzz: /Users/praky/Projects/RUST-practise/10.control_flow/fizzbuzz/src/main.rs
