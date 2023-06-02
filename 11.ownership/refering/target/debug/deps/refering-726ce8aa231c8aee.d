3fadaec1a0cbf86ae9c6cb995685b581aba36e7d
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-02 20:07:44 +0530

    referencing

---
+++ 11.ownership/refering/target/debug/deps/refering-726ce8aa231c8aee.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/11.ownership/refering/target/debug/deps/refering-726ce8aa231c8aee: src/main.rs
+
+/Users/praky/Projects/RUST-practise/11.ownership/refering/target/debug/deps/refering-726ce8aa231c8aee.d: src/main.rs
+
+src/main.rs:
---
+++ 11.ownership/refering/target/debug/refering.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/RUST-practise/11.ownership/refering/target/debug/refering: /Users/praky/Projects/RUST-practise/11.ownership/refering/src/main.rs
