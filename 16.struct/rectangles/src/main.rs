d650ab754f5669afda38a810e655d2e390f5bee9
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-17 15:27:19 +0530

    used formatting of output, tried the dbg! macro and #[derive(Debug)] attribute

--- 16.struct/rectangles/src/main.rs
+++ 16.struct/rectangles/src/main.rs
@@ -1,9 +1,16 @@
-fn main() {
-    let rect1 = (30, 50);
-
-    println!("The area of the rectangle is {}", area(rect1));
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn main() {
+    let rect1 = Rectangle{width: 30, height: 50};
+
+    println!("Printing out the rect1 struct: {:#?}", rect1);
+    dbg!("The area of the rectangle is {}", area(&rect1));
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
