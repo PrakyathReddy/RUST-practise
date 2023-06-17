253d0a95514caae7daaed85c597a42452c9b8f6f
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-17 10:25:41 +0530

    mutable reference

---
+++ 11.ownership/mutable_reference/target/debug/deps/mutable_reference-e865a2813e392dbc.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/11.ownership/mutable_reference/target/debug/deps/mutable_reference-e865a2813e392dbc: src/main.rs
+
+/Users/praky/Projects/RUST-practise/11.ownership/mutable_reference/target/debug/deps/mutable_reference-e865a2813e392dbc.d: src/main.rs
+
+src/main.rs:
---
+++ 11.ownership/mutable_reference/target/debug/mutable_reference.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/RUST-practise/11.ownership/mutable_reference/target/debug/mutable_reference: /Users/praky/Projects/RUST-practise/11.ownership/mutable_reference/src/main.rs
