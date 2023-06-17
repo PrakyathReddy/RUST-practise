1fe8e64a769f83ad0a9c10daf3a7afef87596f67
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-17 15:10:21 +0530

    traditional vs using struct

---
+++ 16.struct/rectangles/target/debug/deps/rectangles-aea44ad9911f7150.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/16.struct/rectangles/target/debug/deps/rectangles-aea44ad9911f7150: src/main.rs
+
+/Users/praky/Projects/RUST-practise/16.struct/rectangles/target/debug/deps/rectangles-aea44ad9911f7150.d: src/main.rs
+
+src/main.rs:
---
+++ 16.struct/rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/RUST-practise/16.struct/rectangles/target/debug/rectangles: /Users/praky/Projects/RUST-practise/16.struct/rectangles/src/main.rs
