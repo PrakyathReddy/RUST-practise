5848edf92afb7201d44535f2b21620a59aa1a401
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-18 09:05:37 +0530

    doing it only with enums instead of creating multiple structs

--- 17.enum/enums/src/main.rs
+++ 17.enum/enums/src/main.rs
@@ -1,32 +1,26 @@
 
 fn main() {
 
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-    route(four);
-    route(six);
-
-    enum IpAddrKind {
-        V4,
-        V6,
+    let _four = IpAddr::V4;
+    let _six = IpAddr::V6;
+    // route(four);
+    // route(six);
+    #[derive(Debug)]
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
+        V6(String),
     }
     
-    struct IpAddr {
-        kind: IpAddrKind,
-        address: String,
-    }
+    // struct IpAddr {
+    //     kind: IpAddrKind,
+    //     address: String,
+    // }
 
-    let _home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let home = IpAddr::V4(127,0,0,1);
+    println!("{:?}",home);
+    let _loopback = IpAddr::V6(String::from("::1"));
 
-    let _loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-
-    fn route(_ip_kind: IpAddrKind) {}
+    // fn route(_ip_kind: IpAddrKind) {}
 }
 
 
