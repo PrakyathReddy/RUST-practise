c93aeff4fa63be1428473915abe4727a6b2126a9
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-19 08:39:16 +0530

    enum within enum, giving expressions as value for match keys

--- 18.match/match_type/src/main.rs
+++ 18.match/match_type/src/main.rs
@@ -3,16 +3,31 @@ enum Coin {
     Penny,
     Nickel,
     Dime, 
-    Quarter,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska, 
+    Washington,
+    California, 
+    NewYork,
 }
 
 #[allow(unused)]
 fn value_in_cents(coin: Coin) -> i32 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!!");
+            1
+        },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
     }
 }
 
