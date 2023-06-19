62df9302543b2909a541af4600d8485993d8491d
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-19 16:48:23 +0530

    use keyword

--- 19.package-crates-modules/restaurant/src/lib.rs
+++ 19.package-crates-modules/restaurant/src/lib.rs
@@ -3,6 +3,7 @@ mod front_of_house {
         pub fn add_to_waitlist() {}
         fn seat_at_table() {}
     }
+
     mod serving {
         fn take_order() {}
         fn serve_order() {}
@@ -10,9 +11,35 @@ mod front_of_house {
     }
 }
 
+pub use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+    // after creating 'use'
+    hosting::add_to_waitlist(); 
+
+    let order1 = back_of_house::Appetizer::Soup;
+}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String;
+        seasonal_fruits: String::from("Peaches"),
+    }
+
+    impl Breakfast {
+        pub fn eat_at_restaurant() {
+            let mut meal = back_of_house::Breakfast::summer("Rye");
+            meal.toast = String::from("Wheat");
+            println!("I'd like {} toast please", meal.host);
+        }
+    }
+
+    pub enum Appetizer {
+        Soup, 
+        Salad,
+    }
 }
---
+++ 19.package-crates-modules/use_keyword/src/lib.rs
@@ -0,0 +1,11 @@
+use std::fmt::Result;
+use std::fmt::Result as IoResult;
+use std::{cmp::Ordering, io};
+
+fn function1() -> Result {
+    // --snip-
+}
+
+fn function2() -> IoResult {
+    // --snip-
+}
