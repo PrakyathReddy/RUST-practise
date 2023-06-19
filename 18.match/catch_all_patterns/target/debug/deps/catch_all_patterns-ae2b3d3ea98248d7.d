df6459d60631376ba2bfac189ed7a130a0b7ba49
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-06-19 09:02:26 +0530

    catch-all patterns and _ placeholder

---
+++ 18.match/catch_all_patterns/target/debug/deps/catch_all_patterns-ae2b3d3ea98248d7.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/18.match/catch_all_patterns/target/debug/deps/catch_all_patterns-ae2b3d3ea98248d7: src/main.rs
+
+/Users/praky/Projects/RUST-practise/18.match/catch_all_patterns/target/debug/deps/catch_all_patterns-ae2b3d3ea98248d7.d: src/main.rs
+
+src/main.rs:
