0e3611f603f9a57d8d8d666984b47ae182c6433a
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-07-04 20:11:45 +0530

    sample program that panics

---
+++ 28.panic/panic/target/debug/deps/panic-553fe9cc177e6ad8.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/28.panic/panic/target/debug/deps/panic-553fe9cc177e6ad8: src/main.rs
+
+/Users/praky/Projects/RUST-practise/28.panic/panic/target/debug/deps/panic-553fe9cc177e6ad8.d: src/main.rs
+
+src/main.rs:
---
+++ 28.panic/panic/target/debug/panic.d
@@ -0,0 +1,1 @@
+/Users/praky/Projects/RUST-practise/28.panic/panic/target/debug/panic: /Users/praky/Projects/RUST-practise/28.panic/panic/src/main.rs
