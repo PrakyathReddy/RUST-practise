a4cf54e304a0f72bbee80e8789526fc3ae0ce52c
Prakyath Reddy <prakyathreddyk@gmail.com>
2023-07-10 22:27:33 +0530

    understanding  unit tests

---
+++ 36.unit_testing/unit_testing/target/debug/deps/unit_testing-516e6a3f4ada50d8.d
@@ -0,0 +1,5 @@
+/Users/praky/Projects/RUST-practise/36.unit_testing/unit_testing/target/debug/deps/unit_testing-516e6a3f4ada50d8: src/lib.rs
+
+/Users/praky/Projects/RUST-practise/36.unit_testing/unit_testing/target/debug/deps/unit_testing-516e6a3f4ada50d8.d: src/lib.rs
+
+src/lib.rs:
---
+++ 36.unit_testing/unit_testing/target/debug/deps/unit_testing-99ad8428fb27c988.d
@@ -0,0 +1,7 @@
+/Users/praky/Projects/RUST-practise/36.unit_testing/unit_testing/target/debug/deps/unit_testing-99ad8428fb27c988.rmeta: src/lib.rs
+
+/Users/praky/Projects/RUST-practise/36.unit_testing/unit_testing/target/debug/deps/libunit_testing-99ad8428fb27c988.rlib: src/lib.rs
+
+/Users/praky/Projects/RUST-practise/36.unit_testing/unit_testing/target/debug/deps/unit_testing-99ad8428fb27c988.d: src/lib.rs
+
+src/lib.rs:
